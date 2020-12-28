e78ac9f4fdc3dfa09d05ad0363f0b04ec8250520
arsen-sheverdin <52337146+arsen-sheverdin@users.noreply.github.com>
2020-12-28 06:27:27 +0600

    cleaned the script

--- search.py
+++ search.py
@@ -3,70 +3,6 @@ import torch
 import numpy as np
 
 device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu") 
-# from main import *
-
-# def model_complexity(model):
-#   pytorch_total_params = sum(p.numel() for p in model.parameters())
-#   print('Total:      \t\t', pytorch_total_params/10**6)
-#   pytorch_train_params = sum(p.numel() for p in model.parameters() if p.requires_grad==True)
-#   print('Trainable:\t\t', pytorch_train_params/10**6)
-#   return (pytorch_train_params/10**6)
-
-# Validation set estimation
-# def val_estimator(model,val_loaer):
-#   # with torch.no_grad():
-#   #   model.eval()
-#   #   val_losses_L1 = []
-#   #   for x_val, y_val in val_loader:
-#   #       model.eval()
-#   #     # x_val, y_val = next(iter(val_loader))
-#   #       x_val = x_val.to(device)
-#   #       y_val = y_val.to(device)
-#   #       y_hat = model(x_val)
-#   #       val_loss = loss_measure(y_val,y_hat)   #L1
-#   #       val_losses_L1.append(val_loss.item()) 
-#   #   print(st.mean(val_losses_L1))
-#   #   return val_losses_L1
-
-#def save_csv(val_temp, threshold_temp, Learning_r, Layer_s ):
- # vals_matlab = pd.DataFrame(val_temp)
- # threshold_matlab = pd.DataFrame(threshold_temp)
-
-
- # name_vals = 'vals_lr_'+str(Learning_r)+'_layer_size_'+str(Layer_s)+'.csv'
- # name_threshold = 'threshold_lr_'+str(Learning_r)+'_layer_size_'+str(Layer_s)+'.csv'
-
- # vals_matlab.to_csv(name_vals,index = False,header = False)
- # threshold_matlab.to_csv(name_threshold,index = False,header = False)
-
- # %cp {name_vals} /content/gdrive/My\ Drive/SiO2_Ag/MODELS/Inverse_Net/
- # %cp {name_threshold} /content/gdrive/My\ Drive/SiO2_Ag/MODELS/Inverse_Net/
-
-# def net_saver(fold, Layer_s, Learning_r, model):
-#   results_dir    = 'Results'
-#   k_fold_number  = 'Group_Number_' + str(fold)
-#   folder_name    =  k_fold_number  + '_lr_' + str(Learning_r) + '_layer_size_'+str(Layer_s)
-
-#   splitting_dir = os.path.join(
-#         results_dir, k_fold_number)
-#   checkpoint_dir = os.path.join(
-#         splitting_dir, folder_name)
-#   # name = 'FoldNum_' + str(fold) + '_lr_'+str(Learning_r)+'_layer_size_'+str(Layer_s)+'.pt'
-#   # print(name,'  Was saved successfully')
-#   # model_save_name = name
-#   # path = F"/content/gdrive/My Drive/SiO2_Ag/MODELS/Inverse_Net/Collection/Models/{model_save_name}" 
-#   # torch.save(model.state_dict(), path)
-#   name = k_fold_number + '_model_lr_'+str(Learning_r) + '_layer_size_'+str(Layer_s)+'.csv'
-#   name = os.path.join(checkpoint_dir, '_model.pt')
-
-# def net_loader(fold, Layer_s,Learning_r):
-#   name = 'FoldNum_' + str(fold) + '_lr_'+str(Learning_r)+'_layer_size_'+str(Layer_s)+'.pt'
-#   model_save_name = name
-#   testm = NN(Layer_s).to(device)
-#   path = F"/content/gdrive/My Drive/SiO2_Ag/MODELS/Inverse_Net/Collection/Models/{model_save_name}"
-#   testm.load_state_dict(torch.load(path))
-#   print(name,'  Was loaded successfully')
-#   return testm 
 
 def decrease(bestmodel):
   bestmodel.eval()
@@ -105,8 +41,6 @@ def decrease(bestmodel):
 
 
 def extract_positive(vals_arr, threshold_arr): 
-  # val_temp = -999*np.ones((300,5))
-  # threshold_temp= +1000*np.ones((300,5))
 
   val_temp = []
   threshold_temp= []  
@@ -133,12 +67,3 @@ def extract_positive(vals_arr, threshold_arr):
   
   return  val_temp, threshold_temp
 
-#Loading
-# def net_loader(Layer_s,Learning_r):
-#   name = 'lr_'+str(Learning_r)+'_layer_size_'+str(Layer_s)+'.pt'
-#   model_save_name = name
-#   testm = NN(Layer_s).to(device)
-#   path = F"/content/gdrive/My Drive/SiO2_Ag/MODELS/Inverse_Net/{model_save_name}"
-#   testm.load_state_dict(torch.load(path))
-#   print(name,'  Was loaded successfully')
-#   return testm
